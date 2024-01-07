6759dbb7d074a054905530deb74220c90f383488
”Erkin <erkenoof@gmail.com>
2024-01-07 15:38:36 +0600

    added a normal format phone number for higher_footer.js

--- shukurfront/src/components/higher_footer/higher_footer.js
+++ shukurfront/src/components/higher_footer/higher_footer.js
@@ -6,9 +6,32 @@ import React, {useState} from "react";
 
 function Higher_footer({services}){
     const [name,nameSet]=useState();
-    const [number,numberSet]=useState();
+    const [number, numberSet] = useState('');
     const [product_name,product_nameSet]=useState();
     const [comment,commentSet]=useState();
+
+    const formatPhoneNumber = (value) => {
+            let formattedValue = value.startsWith('+') ? value : '+' + value;
+
+            const numbersOnly = formattedValue.slice(1).replace(/[^\d]/g, '');
+            formattedValue = '+' + numbersOnly;
+
+        if (numbersOnly.length > 1) {
+           formattedValue = '+' + numbersOnly.substring(0, 1);
+
+            if (numbersOnly.length <= 4) {
+                formattedValue += ' ' + numbersOnly.substring(1);
+            } else if (numbersOnly.length <= 7) {
+                formattedValue += ' ' + numbersOnly.substring(1, 4) + ' ' + numbersOnly.substring(4);
+            } else {
+                formattedValue += ' ' + numbersOnly.substring(1, 4) + ' ' + numbersOnly.substring(4, 7) + ' ' + numbersOnly.substring(7);
+            }
+        }
+        numberSet(formattedValue);
+
+    };
+
+
 
     const review_send=()=>{
         fetch("http://127.0.0.1:8000/api/review",{
@@ -42,7 +65,12 @@ function Higher_footer({services}){
                             <p>В таком случае заполни заявку и мы добавим его в наш список</p>
                             <form>
                                 <input placeholder={"Имя"} onChange={e => nameSet(e.target.value)}/>
-                                <input placeholder={"Телефон"} type={"number"} onChange={e => numberSet(e.target.value)}/>
+                                <input
+                                placeholder="+7XXXXXXXX"
+                                type="tel"
+                                value={number}
+                                onChange={e => formatPhoneNumber(e.target.value)}
+                                maxLength={15}/>
                                 <input placeholder={"Название товара"} onChange={e => product_nameSet(e.target.value)}/>
                                 <textarea placeholder={"Комментарий"} onChange={e => commentSet(e.target.value)}/>
                                 <button onClick={()=>review_send()} type={"button"}>Отправить</button>
