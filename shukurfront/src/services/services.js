7ac801b7185d894de58ca7f97df064e162c1f61b
itdecision <batyrnurhan@gmail.com>
2024-06-03 14:09:33 +0500

    settings front

--- shukurfront/src/services/services.js
+++ shukurfront/src/services/services.js
@@ -1,50 +1,60 @@
 const token = localStorage.getItem("token");
 
 class Services {
-    GetResource = async (url = "") => {
-        let res;
-        if (token !== "undefined" && token !== null && token !== "") {
-            res = await fetch(`http://91.228.154.48:8000/${url}`, {
-                headers: {
-                    Authorization: `Token ${token}`
-                },
-                //credentials: "include"
-            });
-        } else {
-            res = await fetch(`http://91.228.154.48:8000/${url}`);
-        }
-        if (!res.ok) {
-            throw new Error("чел пиши нормально");
-        }
-        return await res.json();
-    }
+  GetResource = async (url = "") => {
+    let res;
+    try {
+      if (token !== "undefined" && token !== null && token !== "") {
+        res = await fetch(`http://91.228.154.48:8000/${url}`, {
+          headers: {
+            Authorization: `Token ${token}`
+          },
+          //credentials: "include"
+        });
+      } else {
+        res = await fetch(`http://91.228.154.48:8000/${url}`);
+      }
 
-    SendResource = async (url = "", body, header = "") => {
-        let res;
-        if (token !== "undefined" && token !== null && token !== "") {
-            res = await fetch(`http://91.228.154.48:8000/${url}`, {
-                method: 'POST',
-                headers: {
-                    Authorization: `Token ${token}`,
-                    'Content-Type': 'application/json'
-                },
-                body: body
-                //credentials: "include"
-            });
-        } else {
-            res = await fetch(`http://91.228.154.48:8000/${url}`, {
-                method: 'POST',
-                body: body,
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-            });
-        }
-        if (!res.ok) {
-            throw new Error("Error check");
-        }
-        return await res.json();
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return await res.json();
+    } catch (error) {
+      throw new Error("Fetch error: " + error.message);
     }
+  }
+
+  SendResource = async (url = "", body, header = "") => {
+    let res;
+    try {
+      if (token !== "undefined" && token !== null && token !== "") {
+        res = await fetch(`http://91.228.154.48:8000/${url}`, {
+          method: 'POST',
+          headers: {
+            Authorization: `Token ${token}`,
+            'Content-Type': 'application/json'
+          },
+          body: body
+          //credentials: "include"
+        });
+      } else {
+        res = await fetch(`http://91.228.154.48:8000/${url}`, {
+          method: 'POST',
+          body: body,
+          headers: {
+            'Content-Type': 'application/json'
+          },
+        });
+      }
+
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return await res.json();
+    } catch (error) {
+      throw new Error("Fetch error: " + error.message);
+    }
+  }
 }
 
 export default Services;
