b41cfe016fea372979b602274a6c8e823e8b6e66
itdecision <batyrnurhan@gmail.com>
2024-06-03 08:16:41 +0500

    settings

--- shukurWeb/shukurWeb/settings.py
+++ shukurWeb/shukurWeb/settings.py
@@ -9,7 +9,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -107,6 +107,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
