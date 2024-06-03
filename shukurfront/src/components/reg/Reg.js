2ba51f806fbd99e279363f66f13f87e443d4008d
itdecision <batyrnurhan@gmail.com>
2024-06-03 14:24:05 +0500

    fix login and reg

--- shukurfront/src/components/reg/Reg.js
+++ shukurfront/src/components/reg/Reg.js
@@ -1,76 +1,79 @@
-import vector from "./Vector.svg"
-import "./Reg.css"
-import {useState} from "react";
-import 'react-phone-number-input/style.css'
-import Input from 'react-phone-number-input/input'
+import vector from "./Vector.svg";
+import "./Reg.css";
+import { useState } from "react";
+import 'react-phone-number-input/style.css';
+import Input from 'react-phone-number-input/input';
 
-function Reg(){
-    const [login, loginSet] = useState()
-    const [full_name, full_nameSet] = useState()
-    const [email, emailSet] = useState()
-    const [number, setNumber] = useState('')
-    const [password, passwordSet] = useState()
-    const [password_r, password_r_Set] = useState()
-    const [eror, erorSet] = useState("")
-    const [reg_t,reg_tSet] = useState(false);
+function Reg() {
+    const [login, loginSet] = useState("");
+    const [full_name, full_nameSet] = useState("");
+    const [email, emailSet] = useState("");
+    const [number, setNumber] = useState("");
+    const [password, passwordSet] = useState("");
+    const [password_r, password_r_Set] = useState("");
+    const [eror, erorSet] = useState("");
+    const [reg_t, reg_tSet] = useState(false);
 
-    const RegSubmit=()=>{
-        reg_tSet(!reg_t)
+    const RegSubmit = () => {
+        reg_tSet(!reg_t);
     }
 
-    const PostUser=async (user)=> {
+    const PostUser = async () => {
         if (password === password_r) {
-            return await fetch(' http://127.0.0.1:8000/auth/users/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    username: login,
-                    email: email,
-                    password: password,
-                    full_name: full_name,
-                    phone_number: number,
-                    adres: null,
-                    avatar: null
-                })
-            })
-                .then(data=>{
-                    if(data.status !== 201) {
-                        erorSet("Ошибка")
-                    }
-                    else document.location.reload()
-                })
-                .catch(eror=>{
-                    erorSet(eror)
-                })
-        }
-        else{
-            erorSet("пароли не совпадают")
+            try {
+                const response = await fetch('http://91.228.154.48:8000/auth/users/', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        username: login,
+                        email: email,
+                        password: password,
+                        full_name: full_name,
+                        phone_number: number,
+                        adres: null,
+                        avatar: null
+                    })
+                });
+
+                if (response.status !== 201) {
+                    erorSet("Ошибка при регистрации");
+                } else {
+                    document.location.reload();
+                }
+            } catch (error) {
+                erorSet("Ошибка соединения: " + error.message);
+            }
+        } else {
+            erorSet("Пароли не совпадают");
         }
     }
-    return(
+
+    return (
         <div>
-            {reg_t ? (<div className={"reg"}>
+            {reg_t ? (
+                <div className={"reg"}>
                     <div className={"reg_card"}>
                         <h2>Регистрация</h2>
                         <form>
-                            <input placeholder={"Логин"} id={"login"} onChange={e=>loginSet(e.target.value)}/>
-                            <input placeholder={"ФИО"} id={"Full_name"} onChange={e=>full_nameSet(e.target.value)}/>
-                            <input placeholder={"E-mail"} type={"email"} id={"email"} onChange={e=>emailSet(e.target.value)}/>
-                            <Input placeholder="+7XXXXXXXX" value={number} onChange={setNumber}  limitMaxLength={true} maxLength={15}/>
-                            <input placeholder={"Пароль"} type={"password"} id={"password"} onChange={e=>passwordSet(e.target.value)}/>
-                            <input placeholder={"Повторите пароль"} type={"password"} id={"password_review"} onChange={e=>password_r_Set(e.target.value)}/>
-                            <button type={"button"} onClick={()=>PostUser()}>Зарегестрировать</button>
+                            <input placeholder={"Логин"} id={"login"} onChange={e => loginSet(e.target.value)} />
+                            <input placeholder={"ФИО"} id={"Full_name"} onChange={e => full_nameSet(e.target.value)} />
+                            <input placeholder={"E-mail"} type={"email"} id={"email"} onChange={e => emailSet(e.target.value)} />
+                            <Input placeholder="+7XXXXXXXX" value={number} onChange={setNumber} limitMaxLength={true} maxLength={15} />
+                            <input placeholder={"Пароль"} type={"password"} id={"password"} onChange={e => passwordSet(e.target.value)} />
+                            <input placeholder={"Повторите пароль"} type={"password"} id={"password_review"} onChange={e => password_r_Set(e.target.value)} />
+                            <button type={"button"} onClick={() => PostUser()}>Зарегистрировать</button>
                         </form>
-                        <h1>{eror ? eror :""}</h1>
-                        <img src={vector} onClick={()=>RegSubmit()}/>
+                        <h1>{eror}</h1>
+                        <img src={vector} onClick={() => RegSubmit()} alt="Close" />
                     </div>
-                </div>):
-                (<h3 onClick={()=>RegSubmit()}>Регистрация</h3>)}
+                </div>
+            ) : (
+                <h3 onClick={() => RegSubmit()}>Регистрация</h3>
+            )}
         </div>
-    )
-
+    );
 }
 
-export default Reg
+export default Reg;
--- shukurfront/src/components/reg/auth.js
+++ shukurfront/src/components/reg/auth.js
@@ -4,66 +4,74 @@ import React, {useState} from "react";
 import Reg from "./Reg";
 
 function Auth(){
-    const [password,passwordSet]=useState()
-    const [login,loginSet]=useState()
-    const [error,erorSet]=useState()
+    const [password, passwordSet] = useState("");
+    const [login, loginSet] = useState("");
+    const [error, errorSet] = useState(null);
 
-    const [log_t,log_tSet]=useState(false);
-    const LogSubmit=()=>{
-        log_tSet(!log_t)
+    const [log_t, log_tSet] = useState(false);
+    const LogSubmit = () => {
+        log_tSet(!log_t);
     }
-    const auth=()=>{
-        fetch("http://127.0.0.1:8000/auth/token/login/",{
+
+    const auth = () => {
+        fetch("http://91.228.154.48:8000/auth/token/login/", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body:JSON.stringify({
+            body: JSON.stringify({
                 password: password,
                 username: login
             })
-
         })
-            .then(data=>{
-                if(data.status !== 200){
-                    erorSet("Ошибка")
-                    console.log("eror")
-                }
-
-                else{
-                    return data.json()
-                }
-            })
-            .then(res=>{
-                console.log(res.auth_token)
-                localStorage.setItem("token",res.auth_token)
-                document.location.reload()
-            })
+        .then(data => {
+            if (data.status !== 200) {
+                errorSet("Ошибка");
+                console.log("error");
+                return null;
+            } else {
+                return data.json();
+            }
+        })
+        .then(res => {
+            if (res) {
+                console.log(res.auth_token);
+                localStorage.setItem("token", res.auth_token);
+                document.location.reload();
+            }
+        })
+        .catch(err => {
+            errorSet("Ошибка соединения");
+            console.log(err);
+        });
     }
+
     return(
         <div>
-            {log_t ?(
-                    <div>
-                        <p onClick={()=>LogSubmit()}>Авторизация</p>
-                        <div className={"reg"}>
-                            <div className={"reg_card"}>
-                                <h2>Вход</h2>
-                                <form>
-                                    <input placeholder={"Логин или E-mail"} id={"login"}
-                                           onChange={e => loginSet(e.target.value)}/>
-                                    <input placeholder={"Пароль"} type={"password"} id={"password"}
-                                           onChange={e => passwordSet(e.target.value)}/>
-                                    <button type={"button"} onClick={()=>auth()}>Войти</button>
-                                    <h3><Reg /></h3>
-                                </form>
-                                <img src={vector} onClick={() => LogSubmit()}/>
-                            </div>
+            {log_t ? (
+                <div>
+                    <p onClick={() => LogSubmit()}>Авторизация</p>
+                    <div className={"reg"}>
+                        <div className={"reg_card"}>
+                            <h2>Вход</h2>
+                            <form>
+                                <input placeholder={"Логин или E-mail"} id={"login"}
+                                       onChange={e => loginSet(e.target.value)} />
+                                <input placeholder={"Пароль"} type={"password"} id={"password"}
+                                       onChange={e => passwordSet(e.target.value)} />
+                                <button type={"button"} onClick={() => auth()}>Войти</button>
+                                {error && <div className="error">{error}</div>}
+                                <h3><Reg /></h3>
+                            </form>
+                            <img src={vector} onClick={() => LogSubmit()} alt="Close" />
                         </div>
-                    </div>):
-                <p onClick={()=>LogSubmit()}>Авторизация</p>}
+                    </div>
+                </div>
+            ) : (
+                <p onClick={() => LogSubmit()}>Авторизация</p>
+            )}
         </div>
-
     )
 }
 
-export default Auth
+export default Auth;
