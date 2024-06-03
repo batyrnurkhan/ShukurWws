11e88595f56da0bbd7fe4bdcb250a397a4b3c515
itdecision <batyrnurhan@gmail.com>
2024-06-03 14:01:29 +0500

    settings front

--- shukurfront/src/services/services.js
+++ shukurfront/src/services/services.js
@@ -1,47 +1,50 @@
-const token=localStorage.getItem("token")
-class Services{
-    GetResource=async (url="")=>{
-        let res
-        if (token !== "undefined" && token !== null && token!== "" ){
-            res=await fetch(`http://127.0.0.1:8000/${url}`, {
+const token = localStorage.getItem("token");
+
+class Services {
+    GetResource = async (url = "") => {
+        let res;
+        if (token !== "undefined" && token !== null && token !== "") {
+            res = await fetch(`http://91.228.154.48:8000/${url}`, {
                 headers: {
                     Authorization: `Token ${token}`
                 },
-                //credentials:"include"
-            })}
-        else {
-            res=await fetch(`http://127.0.0.1:8000/${url}`)
+                //credentials: "include"
+            });
+        } else {
+            res = await fetch(`http://91.228.154.48:8000/${url}`);
         }
-        if (!res.ok){
-            throw new Error("чел пиши нормально")
+        if (!res.ok) {
+            throw new Error("чел пиши нормально");
         }
         return await res.json();
     }
-    SendResource=async (url="",body,header="")=>{
-        let res
-        if (token !== "undefined" && token !== null && token!== "" ){
-            res=await fetch(`http://127.0.0.1:8000/${url}`, {
+
+    SendResource = async (url = "", body, header = "") => {
+        let res;
+        if (token !== "undefined" && token !== null && token !== "") {
+            res = await fetch(`http://91.228.154.48:8000/${url}`, {
                 method: 'POST',
                 headers: {
                     Authorization: `Token ${token}`,
                     'Content-Type': 'application/json'
                 },
-                body:body
-                //credentials:"include"
-            })}
-        else {
-            res=await fetch(`http://127.0.0.1:8000/${url}`,{
+                body: body
+                //credentials: "include"
+            });
+        } else {
+            res = await fetch(`http://91.228.154.48:8000/${url}`, {
                 method: 'POST',
-                body:body,
+                body: body,
                 headers: {
                     'Content-Type': 'application/json'
                 },
-            })
+            });
         }
-        if (!res.ok){
-            throw new Error("чел пиши нормально")
+        if (!res.ok) {
+            throw new Error("Error check");
         }
         return await res.json();
     }
 }
-export default Services
+
+export default Services;
